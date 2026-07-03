bfccdea878ee247796ef158b433c4c293bccec45
Hritik Das <hritikdas2003@gmail.com>
2026-07-03 20:19:37 +0000

    Implemented Employee Management

---
+++ HandsOn_Exercises/Ex_7_Financial_Forcasting.java
@@ -0,0 +1,24 @@
+package HandsOn_Exercises;
+
+public class Ex_7_Financial_Forcasting {
+
+    static double futureValue(double amount, double rate, int yrs) {
+
+        if (yrs == 0) {
+            return amount;
+        }
+
+        return futureValue(amount * (1 + rate), rate, yrs - 1);
+    }
+    public static void main(String[] args) {
+
+        double amount = 1000;
+        double rate = 0.10;   // 10%
+        int yrs = 3;
+
+        double result = futureValue(amount, rate, yrs);
+
+        System.out.println("Future Value = " + result);
+    }
+
+    }
